db4054a1fa8bbf7700565c5a2bfd3859f5f48c1f
John Boyd <jab7bp@virginia.edu>
2017-07-17 12:01:47 -0400

    Initial Commit

--- BNCPulser.cs
+++ BNCPulser.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CMBTester
 {
-    public partial class Form1 : Form
+    public partial class BNCPulser : Form
     {
-        public Form1()
+        public BNCPulser()
         {
             InitializeComponent();
         }
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace CMBTester
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BNCPulser());
         }
     }
 }
